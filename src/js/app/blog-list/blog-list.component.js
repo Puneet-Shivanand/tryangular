bbd438da512f4e2651956d7f810a0f1cbaab144d
Puneet_s <puneet.as@leucinerichbio.com>
2016-12-24 18:08:50 +0530

    ng-repeat

--- src/js/app/blog-list/blog-list.component.js
+++ src/js/app/blog-list/blog-list.component.js
@@ -20,6 +20,15 @@ angular.module('blogList').
 		// template:'<div ><h1 class="new-class">{{ title }}</h1><button ng-click="Test()">Click me!</button></div>',
 		templateUrl:'/templates/blog-list.html',
 		controller:function($scope){
+			var Items = [
+			{title:"Some title", id:1, desc:"This is a Book"},
+				{title:"No title", id:2, desc:"This to is a Book"},
+					{title:"Some title1", id:3, desc:"This too is a Book"},
+						{title:"Some title2", id:4, desc:"This is yet another Book"},
+			] 
+
+			$scope.items=Items
+
 			console.log("hello")
 	 		$scope.title = 'Hi there'
 			$scope.clicks=0
