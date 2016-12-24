ad98cbf835e74f47f59876d960a87a10872175a2
Puneet_s <puneet.as@leucinerichbio.com>
2016-12-24 16:26:16 +0530

    A controller and component

--- src/js/app/blog-list/blog-list.component.js
+++ src/js/app/blog-list/blog-list.component.js
@@ -1,7 +1,32 @@
 'use strict';
+// controller
+// angular.module('blogList').
+// 	controller('BlogListController', function($scope){
+// 		console.log("hello")
+// 		$scope.title = 'Hi there'
+// 		$scope.clicks=0
+// 		$scope.Test = function(){
+// 			console.log("Clicked")
+// 			//$scope.title = 'Clicked'
+// 			$scope.clicks+=1
+// 			$scope.title = 'Clicked ' + $scope.clicks + ' times'
+// 		}
+// 	});
+
+//component
 
 angular.module('blogList').
-	controller('BlogListController', function($scope){
-		console.log("hello")
-		$scope.title = 'Hi there'
+	component('blogList',{
+		template:'<div ><h1 class="new-class">{{ title }}</h1><button ng-click="Test()">Click me!</button></div>',
+		controller:function($scope){
+			console.log("hello")
+	 		$scope.title = 'Hi there'
+			$scope.clicks=0
+			$scope.Test = function(){
+			console.log("Clicked")
+			$scope.title = 'Clicked'
+			$scope.clicks+=1
+			$scope.title = 'Clicked ' + $scope.clicks + ' times'
+			}
+		}
 	});
